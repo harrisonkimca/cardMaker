188a733a4e228e78d8495b10ba14b98bb0a4d57c
Shreve-La <shrevela@gmail.com>
2017-08-12 20:04:16 -0400

    frame view shmushed in

--- CardViewController.swift
+++ CardViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 import os.log
 
-class CardViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class CardViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+UICollectionViewDataSource, UICollectionViewDelegate {
+    
+
     
     // MARK: Properties
     @IBOutlet weak var teamTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    // MARK: Frame CollectionView Properties
+    @IBOutlet weak var compositeFrame: UIImageView!
+    @IBOutlet weak var frameCollectionView: UICollectionView!
+    
+    // MARK: Overlay imageView, HOOK UP to frame image from cell
+//    @IBOutlet weak var compositeFrame: UIImageView!
+
+    //MARK Generate Frame Data
+    var seedData: SeedData!
+    
     
     var card: Card?
     
@@ -31,10 +45,57 @@ class CardViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
             nameTextField.text = card.name
             photoImageView.image = card.photo
         }
+
+        //MARK Frame Collection Data
+        seedData = SeedData()
+
+        
         
         updateSaveButtonState()
     }
 
+    // MARK: Frame CollectionView Data Source
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return seedData.frames.count
+    }
+    
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:"Cell", for: indexPath)
+        if let frameCell = cell as? FrameCollectionViewCell {
+            frameCell.frameCellImage.image
+                = seedData.frames[indexPath.row].frameImage
+            
+            
+//MARK TODO - replace steve with harrison's data to hook up selected backgroung image 
+            frameCell.backgroundView = UIImageView(image: UIImage(named:"Steve"))
+            return frameCell
+            
+        }
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        compositeFrame.image = seedData.frames[indexPath.row].frameImage
+        
+        //TODO: Write function to set elements of CompositeView
+        //        if let newCard = seedData.frames[indexPath.row] as? Card{
+        //        compositeFrame.image = newCard.baseImage
+        //        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     // MARK: UITextFieldDelegate
     func textFieldDidBeginEditing(_ textField: UITextField) {
         
