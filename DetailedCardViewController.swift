0e3ab53450ae5bc03c0c84d387ef5113587f79ce
Shreve-La <shrevela@gmail.com>
2017-08-21 21:57:54 -0400

    removed stray character

--- DetailedCardViewController.swift
+++ DetailedCardViewController.swift
@@ -26,7 +26,7 @@ class DetailedCardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        8
+        
         frontImage = card?.pngImage
         frontImageView.image = frontImage
         backImage = card?.backpngImage
